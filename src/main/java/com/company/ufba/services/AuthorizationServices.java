0915d869dd8af8be8116104a338ee55ce55caa80
VitaminaNescau <vitorjss82@gmail.com>
2023-07-27 14:23:20 -0300

    add newRequest

--- src/main/java/com/company/ufba/services/AuthorizationServices.java
+++ src/main/java/com/company/ufba/services/AuthorizationServices.java
@@ -18,21 +18,23 @@ public class AuthorizationServices {
 
     private final String urlData = "https://siac.ufba.br/SiacWWW/Welcome.do";
     private final  String urlLogin = "https://siac.ufba.br/SiacWWW/LogonSubmit.do";
-    final String urlNotes ="https://siac.ufba.br/SiacWWW/LogonSubmit.do";
+    private final String urlNotes ="https://siac.ufba.br/SiacWWW/ConsultarCoeficienteRendimento.do";
 
     public List<?> login(User user) {
         try {
            Connection.Response responseData = Jsoup.connect(urlData).get().connection().response();
            Connection.Response responseLogin = Jsoup.connect(urlLogin)
-                    .cookies(responseData.cookies())
-                    .data(user.getLoginInfo())
-                    .post().connection().response();
-            Document doc = Jsoup.connect(urlNotes)
-                    .cookies(responseLogin.cookies())
-                    .headers(responseLogin.headers())
-                    .get();
-            System.out.println(doc);
-           Logger.getLogger("Nova sessão").info(responseLogin.statusMessage());
+                   .newRequest().timeout(5000)
+                   .cookies(responseData.cookies())
+                   .data(user.getLoginInfo())
+                   .post().connection().response();
+           Document doc = Jsoup.connect(urlNotes)
+                   .newRequest()
+                   .cookies(responseLogin.cookies())
+                   .headers(responseLogin.headers())
+                   .get();
+            System.out.println(doc.getElementsByClass("even").tagName("td"));
+           Logger.getLogger("Nova sessão").info(responseLogin.cookies().values().toString());
            return List.of(responseLogin.cookies(),responseLogin.headers());
         } catch (IOException | ValidationException e) {
             Logger.getLogger("Login").info("Login error: " + e.getMessage());
