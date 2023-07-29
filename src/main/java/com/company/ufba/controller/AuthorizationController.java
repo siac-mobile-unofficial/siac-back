1bc038d780efe60798996aa63ffc4789dabbe8da
VitaminaNescau <vitorjss82@gmail.com>
2023-07-28 23:13:51 -0300

    added CORS

--- src/main/java/com/company/ufba/controller/AuthorizationController.java
+++ src/main/java/com/company/ufba/controller/AuthorizationController.java
@@ -7,20 +7,22 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Map;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("auth")
 public class AuthorizationController {
+
     @PostMapping("login")
     public ResponseEntity<?> userLogin( @Valid @RequestBody User user){
         user.setLoginInfo(Map.of("cpf", user.getRegister()
                                 ,"senha",user.getPassword()
                                 ,"x","43"
-                                ,"y","6"));
+                                ,"y","4"));
         return ResponseEntity.ok(new AuthorizationServices().login(user));
     }
-    @GetMapping("oi")
-    public String oi(){
-    return "oi";
+
+    @GetMapping("/teste")
+    public String teste(){
+        return "teste";
     }
 }
