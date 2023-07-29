aafbc9cb55e9b3bb409e63b78ba92c9023912c55
VitaminaNescau <vitorjss82@gmail.com>
2023-07-28 23:15:04 -0300

    clean coding

--- src/main/java/com/company/ufba/services/AuthorizationServices.java
+++ src/main/java/com/company/ufba/services/AuthorizationServices.java
@@ -1,7 +1,6 @@
 package com.company.ufba.services;
 
 import com.company.ufba.dto.User;
-import com.company.ufba.security.TokenService;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.ValidationException;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 @Service
@@ -22,19 +20,14 @@ public class AuthorizationServices {
 
     public List<?> login(User user) {
         try {
+
            Connection.Response responseData = Jsoup.connect(urlData).get().connection().response();
            Connection.Response responseLogin = Jsoup.connect(urlLogin)
                    .newRequest().timeout(5000)
                    .cookies(responseData.cookies())
                    .data(user.getLoginInfo())
                    .post().connection().response();
-           Document doc = Jsoup.connect(urlNotes)
-                   .newRequest()
-                   .cookies(responseLogin.cookies())
-                   .headers(responseLogin.headers())
-                   .get();
-            System.out.println(doc.getElementsByClass("even").tagName("td"));
-           Logger.getLogger("Nova sessão").info(responseLogin.cookies().values().toString());
+           Logger.getLogger("Nova sessão").info("Cookies: "+responseLogin.cookies().values().toString());
            return List.of(responseLogin.cookies(),responseLogin.headers());
         } catch (IOException | ValidationException e) {
             Logger.getLogger("Login").info("Login error: " + e.getMessage());
