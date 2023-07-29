9b8b409c155b6274e2d77c1254ed3a2eff04908b
VitaminaNescau <vitorjss82@gmail.com>
2023-07-28 23:15:31 -0300

    added Cors configuration

--- src/main/java/com/company/ufba/UfbaApplication.java
+++ src/main/java/com/company/ufba/UfbaApplication.java
@@ -5,6 +5,9 @@ package com.company.ufba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,6 +16,16 @@ public class UfbaApplication {
 	public static void main(String[] args)  {
 		SpringApplication.run(UfbaApplication.class, args);
 
-	}
 
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigure(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*").allowedOrigins("http://localhost:8080");
+
+			}
+		};
+	}
 }
