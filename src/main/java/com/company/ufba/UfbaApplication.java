80c1baaf0b44557cf16041fd2ab783a9db3a152f
vitaminanescau <vitorjss82@gmail.com>
2023-08-03 01:51:30 -0300

    remove bean to corsConfigure

--- src/main/java/com/company/ufba/UfbaApplication.java
+++ src/main/java/com/company/ufba/UfbaApplication.java
@@ -16,16 +16,6 @@ public class UfbaApplication {
 	public static void main(String[] args)  {
 		SpringApplication.run(UfbaApplication.class, args);
 
-
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigure(){
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*").allowedOrigins("http://localhost:8080");
 
-			}
-		};
-	}
 }
