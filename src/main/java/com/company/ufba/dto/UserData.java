c5814a86ea04b0f5ac2b5855c9cae0b20c43b3ca
vitaminanescau <vitorjss82@gmail.com>
2023-08-03 01:52:06 -0300

    datas of user

---
+++ src/main/java/com/company/ufba/dto/UserData.java
@@ -0,0 +1,19 @@
+package com.company.ufba.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class UserData {
+    private String register;
+    private String name;
+    private String couser;
+    private String year;
+    private String cr;
+
+}
