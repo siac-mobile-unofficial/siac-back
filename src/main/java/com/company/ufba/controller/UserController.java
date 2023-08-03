6b0b892d4191b1824ffec443d5e28242b0c53d48
vitaminanescau <vitorjss82@gmail.com>
2023-08-03 01:51:56 -0300

    add endpoint for information the user

--- src/main/java/com/company/ufba/controller/UserController.java
+++ src/main/java/com/company/ufba/controller/UserController.java
@@ -6,12 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("user")
 public class UserController {
     @PostMapping("notes")
     public ResponseEntity<?> findNotes(@Valid @RequestBody Response response){
         return ResponseEntity.accepted().body(new UserServices().NotesForStudent(response));
+    }
+    @PostMapping("/info")
+    public ResponseEntity<?> findInfoUser(@Valid @RequestBody Response response){
+        return ResponseEntity.ok(new UserServices().findUser(response));
     }
 }
