e5eff231fb576c6368499a4f4ba2919b0239ddda
vitaminanescau <vitorjss82@gmail.com>
2023-08-03 01:51:05 -0300

    remove atribute Response

--- src/main/java/com/company/ufba/dto/Response.java
+++ src/main/java/com/company/ufba/dto/Response.java
@@ -1,13 +1,15 @@
 package com.company.ufba.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.util.Map;
 @Getter @Setter @NoArgsConstructor
 public class Response {
-
     private Map<String,String> cookies;
+
     private Map<String,String> headers;
 }
