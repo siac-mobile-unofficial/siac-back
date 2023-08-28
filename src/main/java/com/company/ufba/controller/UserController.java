d7b5222dcaf8182f78aeac285e6b5e083bd84943
vitaminanescau <vitorjss82@gmail.com>
2023-08-27 23:06:12 -0300

    add pdf data

--- src/main/java/com/company/ufba/controller/UserController.java
+++ src/main/java/com/company/ufba/controller/UserController.java
@@ -2,11 +2,13 @@ package com.company.ufba.controller;
 
 import com.company.ufba.dto.Response;
 import com.company.ufba.services.UserServices;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://dev.com")
 @RestController
 @RequestMapping("user")
 public class UserController {
@@ -25,5 +27,11 @@ public class UserController {
     @PostMapping("/classroom")
     public ResponseEntity<?> findClass(@Valid @RequestBody Response response){
         return ResponseEntity.ok(new UserServices().findClassroom(response));
+    }
+    @PostMapping(value = "/pdf",produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<?> findPDF(@Valid @RequestBody Response response){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Disposition","attachment; filename=Comprovante_Matricula.pdf");
+        return ResponseEntity.ok().headers(headers).body(new UserServices().findPDF(response));
     }
 }
