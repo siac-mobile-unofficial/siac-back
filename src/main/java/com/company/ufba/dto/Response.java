5a21c2812275e540835e5ede88cb1a614e81f2f1
vitaminanescau <vitorjss82@gmail.com>
2023-09-05 14:06:31 -0300

    [FIX] issues import

--- src/main/java/com/company/ufba/dto/Response.java
+++ src/main/java/com/company/ufba/dto/Response.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
 import java.util.Map;
 @Getter @Setter @NoArgsConstructor
 public class Response {
