f07aa652f92f3bb4c5d3ee35f264cc39c0b0c771
vitaminanescau <vitorjss82@gmail.com>
2023-09-13 02:46:08 -0300

    [WIP] Add anotation in MAP

--- src/main/java/com/company/ufba/dto/User.java
+++ src/main/java/com/company/ufba/dto/User.java
@@ -1,8 +1,10 @@
 package com.company.ufba.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 
 
@@ -15,6 +17,6 @@ public class User {
 
     private String register;
     private String password;
-
+    @JsonIgnore
     private Map<String, String> loginInfo = new HashMap<>();
 }
