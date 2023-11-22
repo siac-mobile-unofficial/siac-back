7d998aa66147b9f928e1558cbef891cdc59ad789
VitaminaNescau <vitorjss82@gmail.com>
2023-11-21 22:23:00 -0300

    [FEAT] Login exception

---
+++ src/main/java/com/company/ufba/utils/Exception/custom/LoginException.java
@@ -0,0 +1,10 @@
+package com.company.ufba.utils.Exception.custom;
+
+public class LoginException extends RuntimeException {
+
+
+    public LoginException(String message) {
+        super(message);
+
+    }
+}
