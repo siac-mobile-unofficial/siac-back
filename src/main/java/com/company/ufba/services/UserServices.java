3de2c04652fc8ad4615575e42fd30bce64fe31d5
VitaminaNescau <vitorjss82@gmail.com>
2023-11-21 22:27:19 -0300

    [FEAT] create path file pdf and get pdf to history

--- src/main/java/com/company/ufba/services/UserServices.java
+++ src/main/java/com/company/ufba/services/UserServices.java
@@ -1,6 +1,7 @@
 package com.company.ufba.services;
 
 import com.company.ufba.dto.*;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.ValidationException;
 import org.jsoup.nodes.Document;
@@ -10,11 +11,9 @@ import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.CookieManager;
-import java.net.HttpCookie;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -22,6 +21,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
@@ -29,14 +29,23 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserServices {
+    Logger log = Logger.getLogger("DEV_USER_SERVICES");
     final String urlNotes ="https://siac.ufba.br/SiacWWW/ConsultarCoeficienteRendimento.do";
     final String urlInfo ="https://siac.ufba.br/SiacWWW/ConsultarComprovanteMatricula.do";
     final String urlCurriculumOB = "https://siac.ufba.br/SiacWWW/ConsultarDisciplinasObrigatorias.do";
     final String getUrlCurriculumOP = "https://siac.ufba.br/SiacWWW/ConsultarDisciplinasOptativas.do";
     final String urlPDF = "https://siac.ufba.br/SiacWWW/GerarComprovanteMatricula.do";
-    final String local ="PDF/";
+    final String urlNotePDF = "https://siac.ufba.br/SiacWWW/ImprimirHistoricoEscolar.do";
+    final String urlHistorySchool = "https://siac.ufba.br/SiacWWW/GerarHistorico.do";
+    final Map<String,String> urlLockRegistration =  Map.of("lock","https://siac.ufba.br/SiacWWW/SolicitarTrancamentoMatricula.do"
+    ,"positionLock","https://siac.ufba.br/SiacWWW/VerificarPosicaoTrancamento.do");
+    final String urlSIAC = "https://siac.ufba.br/";
+    final  static String local ="PDF/";
     private Document doc;
 
+    static {
+        if (!new File(local).exists()) new File(local).mkdir();
+    }
     public List<?> ListOfData(Elements element){
         AtomicReference<String> aux = new AtomicReference<>();
         AtomicReference<List<String>> auxM = new AtomicReference<>();
@@ -73,12 +82,11 @@ public class UserServices {
                         case 7:
                             return new Matter(item);
                         default:
-                            Logger.getLogger("ListOfData: ").info("No Identify: "+item);
                             return "No Identify";
                 }
                 })
            .collect(Collectors.toList());
-    }/*metodo só funciona com as tag tr, pois consome as linhas da tabeka*/
+    }/*metodo só funciona com as tag tr, pois consome as linhas da tabela*/
     public List<Matter> NotesForStudent(Response response){
         try {
              doc = Jsoup.connect(urlNotes)
@@ -99,6 +107,7 @@ public class UserServices {
 
     }
     public UserData findUser(Response   response) {
+        System.out.println(response.getCookies().toString());
         try {
             doc = Jsoup.connect(urlInfo)
                     .newRequest()
@@ -112,7 +121,7 @@ public class UserServices {
                             .map(item->item.ownText())
                             .filter(s -> !s.isEmpty())
                             .collect(Collectors.toList());
-
+                Logger.getLogger("Teste").info(user.toString());
                 return new UserData(user.get(0),user.get(1),user.get(2),user.get(3),user.get(4),date[1].trim());
         }catch (IOException e){
             Logger.getLogger("User").info(e.getMessage());
@@ -179,5 +188,46 @@ public class UserServices {
             Logger.getLogger("PDF").info(e.getMessage());
         }
     return null;
+    }
+    public Resource findPDFNotes(){
+
+        return null;
+    }
+    public Resource findPDFHistory(Response response){
+        response.getHeaders().remove("Connection");
+        response.getHeaders().remove("Transfer-Encoding");
+        String finalLocal = local+response.getCookies().get("JSESSIONID")+"_file.pdf";
+        CookieManager CM = new CookieManager();
+        HttpCookie cookie = new HttpCookie("JSESSIONID",response.getCookies().get("JSESSIONID"));
+        cookie.setPath("/");
+        cookie.setVersion(1);
+        try {
+            CM.getCookieStore().add(new URI(urlSIAC),cookie);
+        HttpClient client = HttpClient.newBuilder().cookieHandler(CM).build();
+        HttpRequest.Builder request = HttpRequest.newBuilder(new URI(urlHistorySchool));
+        response.getHeaders().forEach(request::setHeader);
+        HttpRequest req = request.build();
+        HttpResponse res = client.send(req, HttpResponse.BodyHandlers.ofFile(Path.of(finalLocal)));
+        log.info(String.valueOf(res.statusCode()));
+        if (res.statusCode() == HttpStatus.OK.value()) return new UrlResource(Path.of(finalLocal).toUri());
+        else {
+            Files.deleteIfExists(Path.of(finalLocal));
+            log.info("DELETADO:"+finalLocal);
+        };
+        return null;
+        }catch (URISyntaxException | IOException | InterruptedException e){}
+        return null;
+    }
+    public List<?> findLockRegister(Response response){
+        try {
+            doc = Jsoup.connect(urlLockRegistration.get("lock"))
+                    .cookies(response.getCookies())
+                    .headers(response.getHeaders()).get();
+            System.out.println(doc);
+            return null;
+        }catch (IOException e){
+            return null;
+        }
+
     }
 }
