df20090536f6752271ce826e4521347971d24691
VitaminaNescau <vitorjss82@gmail.com>
2023-11-21 22:21:11 -0300

    [FEAT] Handler exception

---
+++ src/main/java/com/company/ufba/utils/Exception/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.company.ufba.utils.Exception;
+
+import com.company.ufba.utils.Exception.custom.LoginException;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.logging.Logger;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(value = { LoginException.class})
+    public ResponseEntity<Object> loginError(LoginException ex) {
+        Logger.getGlobal().info(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(buildErrorResponse(HttpStatus.UNAUTHORIZED,ex));
+    }
+    private static ErrorResponse buildErrorResponse(final HttpStatus status, final RuntimeException ex) {
+        return new ErrorResponse(status.value(), ex.getMessage());
+    }
+}
