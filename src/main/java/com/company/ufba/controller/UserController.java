b199f46f5746bb4e343bca4cce3a6495572d17e5
VitaminaNescau <vitorjss82@gmail.com>
2023-11-23 01:27:17 -0300

    [REFACTOR] use cookie value actual and clean code for boost

--- src/main/java/com/company/ufba/controller/UserController.java
+++ src/main/java/com/company/ufba/controller/UserController.java
@@ -3,56 +3,60 @@ package com.company.ufba.controller;
 import com.company.ufba.dto.Response;
 import com.company.ufba.dto.User;
 import com.company.ufba.services.UserServices;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 
-@RestController
+@Controller
 @RequestMapping("user")
 public class UserController {
 
 
-    @PostMapping("notes")
-    public ResponseEntity<?> findNotes(@Valid @RequestBody Response response){
-        return ResponseEntity.accepted().body(new UserServices().NotesForStudent(response));
+
+    @GetMapping("notes")
+    public ResponseEntity<?> findNotes(@CookieValue("JSESSIONID") String cookie) {
+        return ResponseEntity.accepted().body(new UserServices().NotesForStudent(new Response(cookie)));
     }
-//    @GetMapping("/info")
-//    public ResponseEntity<?> findInfoUser(@Valid @RequestBody Response response){
-//        return ResponseEntity.ok(new UserServices().findUser(response));
-//    }
-@GetMapping("/info")
-public ResponseEntity<?> findInfoUser(@RequestHeader("header")Map<String,String>header){
-        var r = new Response();
-        r.setHeaders(header);
-        r.setCookies(Map.of("JSESSIONID",header.get("cookie")));
-    return ResponseEntity.ok(new UserServices().findUser(r));
-}
-    @PostMapping("/curriculum")
-    public ResponseEntity<?> findCurriculum(@Valid @RequestBody Response response){
-        return  ResponseEntity.ok(new UserServices().findCurriculum(response));
+
+    @GetMapping("/info")
+    public ResponseEntity<?> findInfoUser(@CookieValue("JSESSIONID") String cookie) {
+        return ResponseEntity.ok(new UserServices().findUser(new Response(cookie)));
     }
-    @PostMapping("/classroom")
-    public ResponseEntity<?> findClass(@Valid @RequestBody Response response){
-        return ResponseEntity.ok(new UserServices().findClassroom(response));
+
+    @GetMapping("/curriculum")
+    public ResponseEntity<?> findCurriculum(@CookieValue("JSESSIONID") String cookie) {
+        return ResponseEntity.ok(new UserServices().findCurriculum(new Response(cookie)));
     }
-    @PostMapping(value = "/pdf",produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<?> findPDF(@Valid @RequestBody Response response){
+
+    @GetMapping("/classroom")
+    public ResponseEntity<?> findClass(@CookieValue("JSESSIONID") String cookie) {
+        return ResponseEntity.ok(new UserServices().findClassroom(new Response(cookie)));
+    }
+
+    @GetMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<?> findPDF(@CookieValue("JSESSIONID") String cookie) {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Disposition","attachment; filename=Comprovante_Matricula.pdf");
-        return ResponseEntity.ok().headers(headers).body(new UserServices().findPDF(response));
+        headers.set("Content-Disposition", "attachment; filename=Comprovante_Matricula.pdf");
+        return ResponseEntity.ok().headers(headers).body(new UserServices().findPDF(new Response(cookie)));
     }
-    @PostMapping("/lock")
-    public ResponseEntity<?> findLock(@Valid @RequestBody Response response){
+
+    @GetMapping("/lock")
+    public ResponseEntity<?> findLock(@Valid @RequestBody Response response) {
         new UserServices().findLockRegister(response);
-    return null;
+        return null;
     }
-    @PostMapping(value="/history",produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<?> findHistory(@Valid @RequestBody Response response){
-        return ResponseEntity.ok(new UserServices().findPDFHistory(response));
+
+    @GetMapping(value = "/history", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<?> findHistory(@CookieValue("JSESSIONID") String cookie) {
+        return ResponseEntity.ok(new UserServices().findPDFHistory(new Response(cookie)));
     }
 }
