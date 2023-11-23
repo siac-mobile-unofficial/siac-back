e0d3234124fb5fd596efaaf11c6048c688889c80
VitaminaNescau <vitorjss82@gmail.com>
2023-11-23 01:26:22 -0300

    [FEAT] add values for headers and constructor insert cookie

--- src/main/java/com/company/ufba/dto/Response.java
+++ src/main/java/com/company/ufba/dto/Response.java
@@ -1,14 +1,29 @@
 package com.company.ufba.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Map;
-@Getter @Setter @NoArgsConstructor
-public class Response {
-    private Map<String,String> cookies;
 
-    private Map<String,String> headers;
+@Getter
+@Setter
+@NoArgsConstructor
+public class Response {
+    private Map<String, String> cookies;
+    private Map<String, String> headers =
+            Map.of("Server", "Apache-Coyote/1.1",
+                    "Transfer-Encoding", "chunked",
+                    "Content-Type", "text/html;charset=ISO-8859-1",
+                    "Connection", "Keep-Alive, keep-alive",
+                    "Keep-Alive", "timeout=60",
+                    "Content-Encoding", "gzip",
+                    "X-Powered-By", "Servlet 2.4; JBoss-4.0.5.GA (build: CVSTag=Branch_4_0 date=200610162339)/Tomcat-5.5"
+            );
+
+    public Response(String cookie) {
+        this.cookies = Map.of("JSESSIONID", cookie);
+    }
+
+
 }
