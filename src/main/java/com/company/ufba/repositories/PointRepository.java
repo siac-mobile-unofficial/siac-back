654ec0e635743742a880ec03d55731f7c1a0b5bb
VitaminaNescau <vitorjss82@gmail.com>
2023-12-14 17:40:36 -0300

    extra

--- src/main/java/com/company/ufba/repositories/PointRepository.java
+++ src/main/java/com/company/ufba/repositories/PointRepository.java
@@ -4,5 +4,10 @@ import com.company.ufba.buzufba.PointEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
-public interface PointRepository extends JpaRepository<PointEntity,Integer> {}
+public interface PointRepository extends JpaRepository<PointEntity, Integer> {
+    Set<PointEntity> findAllByLocale_LatitudeIsBetweenAndLocale_LongitudeIsBetween(Float latitudeRef, Float latitudeAlgo, Float longitudeRef, Float logintudeAlgo);
+}
