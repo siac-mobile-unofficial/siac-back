33c20823c18d45a1c28a3913ad53c389ab48808b
VitaminaNescau <vitorjss82@gmail.com>
2023-12-29 09:58:15 -0300

    [WIP] add method fo bus

--- src/main/java/com/company/ufba/repositories/BusRepository.java
+++ src/main/java/com/company/ufba/repositories/BusRepository.java
@@ -4,8 +4,8 @@ import com.company.ufba.buzufba.BusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface BusRepository  extends JpaRepository<BusEntity,Integer> {
-
-
+    Set<BusEntity> findBusEntitiesByPoint_Name(String name);
 }
