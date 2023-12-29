41ea90fddc7e73a208af85b5530f803f73c02f52
VitaminaNescau <vitorjss82@gmail.com>
2023-12-29 10:01:25 -0300

    [FEAT] new exception

--- src/main/java/com/company/ufba/utils/Exception/CustomExceptionHandler.java
+++ src/main/java/com/company/ufba/utils/Exception/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.company.ufba.utils.Exception;
 
 import com.company.ufba.utils.Exception.custom.LoginException;
+import com.company.ufba.utils.Exception.custom.RegisterException;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,15 @@ import java.util.logging.Logger;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler(value = {RegisterException.class})
+    public ResponseEntity<Object> registerError(RegisterException ex){
+        Logger.getLogger("Register").info(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(buildErrorResponse(HttpStatus.NOT_FOUND,ex));
+    }
 
     @ExceptionHandler(value = { LoginException.class})
     public ResponseEntity<Object> loginError(LoginException ex) {
-        Logger.getGlobal().info(ex.getMessage());
+        Logger.getLogger("Login").info(ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(buildErrorResponse(HttpStatus.UNAUTHORIZED,ex));
     }
     private static ErrorResponse buildErrorResponse(final HttpStatus status, final RuntimeException ex) {
