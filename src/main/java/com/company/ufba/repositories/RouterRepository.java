23178b445b4d0eb9fca859f223316a8862d54487
VitaminaNescau <vitorjss82@gmail.com>
2023-12-29 22:01:00 -0300

    [WIP] new method

--- src/main/java/com/company/ufba/repositories/RouterRepository.java
+++ src/main/java/com/company/ufba/repositories/RouterRepository.java
@@ -4,6 +4,9 @@ import com.company.ufba.buzufba.RouterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface RouterRepository extends JpaRepository<RouterEntity,Integer> {
+    Set<RouterEntity> findAllById(int id);
 }
