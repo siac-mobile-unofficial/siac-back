4a855d4d5ab65704aa197d075bfa6929461c2764
VitaminaNescau <vitorjss82@gmail.com>
2023-12-29 22:00:27 -0300

    [FEAT] new exception

---
+++ src/main/java/com/company/ufba/utils/Exception/custom/RegisterException.java
@@ -0,0 +1,7 @@
+package com.company.ufba.utils.Exception.custom;
+
+public class RegisterException extends RuntimeException{
+    public RegisterException(String message){
+        super(message);
+    }
+}
