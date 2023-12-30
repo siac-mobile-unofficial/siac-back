706b646fa1b676ae5e7a984fc643ee0da58445dd
VitaminaNescau <vitorjss82@gmail.com>
2023-12-29 22:00:06 -0300

    [REFACTOR] only string

--- src/main/java/com/company/ufba/utils/values/mapsValues.java
+++ src/main/java/com/company/ufba/utils/values/mapsValues.java
@@ -1,8 +1,7 @@
 package com.company.ufba.utils.values;
 
 public enum mapsValues {
-    X1(-0.2,0.2),X2(-0.4,0.4),X3(-6,6),X4(-8,8);
+    X1,X2,X3,X4;
 //TODO ALTERAR VALORES
-    mapsValues(double i, double i1) {
-    }
+
 }
